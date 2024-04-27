89870cba414ac30639cdf1a717f56bbc0bc98ea5
neeraja-anil <neerajaanil1026@gmail.com>
2024-04-27 17:54:27 +0530

    Split server js to app and server for test purposes

--- server/server.js
+++ server/server.js
@@ -1,24 +1,6 @@
-import express from "express";
-import cors from "cors";
-import db from "./models/index.js";
-import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
-import ProjectRoutes from "./routes/projectRoutes.js";
-import TodoRoutes from "./routes/todoRoutes.js";
-import UserRoutes from "./routes/userRoutes.js";
+import { app } from "./app.js";
 
 const PORT = 5000;
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/api/projects", ProjectRoutes);
-app.use("/api/users", UserRoutes);
-app.use("/api/todos", TodoRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
